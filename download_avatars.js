848129e8690ad1cbd4c32e9c8d229f1d802b0e61
Vasiliy Klimkin <vasily.klimkin@gmail.com>
2017-06-05 21:52:41 +0000

    Figured out the User-Agent problem and now getting status 200 OK! which is good because now we are accessing the JSON objects

--- download_avatars.js
+++ download_avatars.js
@@ -1,2 +1,30 @@
-var request = require("request");
-console.log("Welcome to GitHub Avatar Downloader");
+function getRepoContributors(repoOwner, repoName, cb) {
+  var request = require("request");
+  var GITHUB_TOKEN = '8548dbb8652a4ab3d1538e602d887a4c51f02167';
+  var GITHUB_USER = 'waff1e';
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+
+  var options = {
+    url: requestURL,
+    headers: {
+      'User-Agent': 'request'
+    }
+  };
+
+
+  request.get(options)
+          .on("error", function(error) {
+            throw error;
+          })
+          .on("response", function(response) {
+            console.log("Response status: " + respose.statusCode);
+          });
+
+  console.log("Welcome to GitHub Avatar Downloader");
+}
+
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
